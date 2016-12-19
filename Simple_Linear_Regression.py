7defa792e942d2a556771250e81881d20a45516b
James Petullo <pentester18@gmail.com>
2016-12-19 15:28:35 -0500

    Add files via upload

---
+++ Simple_Linear_Regression.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model #this allows us to preform the regression
+data = np.loadtxt("my_data.txt")
+#link for this data: www3.nccu.edu.tw/~hmlien/statistics/lecture/Linear%20Regression%20Example%20Data.pptx
+xs = data[:,0] #our x-coordinate data, whatever it might be
+ys = data[:,1] #our y-coordinate data
+xs = np.array([xs]).T #this turns our x data into an array for processing
+reg = linear_model.LinearRegression()
+
+reg.fit(xs, ys) #trains the data
+a = reg.coef_[0] #the slope of the line of best fit
+b = reg.intercept_ #the y-intercept of the slope of the line of best fit
+ablineValues = [] #this stores the new y-corrdates of the line of best fit
+for i in xs:
+    ablineValues.append(a*i[0]+b)
+print "The slope is: ", a
+print "The intercept is: ", b
+
+plt.scatter(xs, ys)
+plt.plot(xs, ablineValues, 'r')
+plt.show()
